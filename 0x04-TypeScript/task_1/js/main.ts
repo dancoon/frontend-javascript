9ad35d29307b51baa6feaa391b0094ddf0a34f12
dancoon <dancun.ian@students.jkuat.ac.ke>
2025-01-27 12:02:03 +0300

    Writing a class

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,46 @@
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [propName: string]: any;
+}
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (
+  firstName: string,
+  lastName: string
+) => firstName.substring(0, 1) + ". " + lastName;
+
+interface StudentClassInterface {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework() {
+    return "Currently Working";
+  }
+
+  displayName() {
+    return this.firstName;
+  }
+}
